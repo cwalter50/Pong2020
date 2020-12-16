a5d4d46831013a7aaeb033bbdd9e321e5e0f5582
Chris Walter <walt50@gmail.com>
2020-12-16 09:26:12 -0500

    Day 3 Complete: Built Paddle

--- Pong2020/GameScene.swift
+++ Pong2020/GameScene.swift
@@ -10,26 +10,54 @@ import GameplayKit
 
 class GameScene: SKScene
 {
-    
+    var paddle = SKSpriteNode()
+    var ball = SKSpriteNode()
     
     override func didMove(to view: SKView)
     {
+        // get access to the paddle
+        paddle = childNode(withName: "paddle") as! SKSpriteNode
+        
+        // get access to the ball
+        ball = childNode(withName: "ball") as! SKSpriteNode
         
         let borderBody = SKPhysicsBody(edgeLoopFrom: self.frame)
         borderBody.friction = 0.0
         self.physicsBody = borderBody
         
-        physicsWorld.gravity = CGVector(dx: 5, dy: 9.8)
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
 //        physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
 //         physicsWorld.gravity = CGVector(dx: 0.0, dy: -9.8)
 
         
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let location = touches.first?.location(in: self)
-        makeNewBall(loc: location!)
+    var isFingerOnPaddle = false
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let location = touches.first!.location(in: self)
+//        makeNewBall(loc: location!)
+        if paddle.frame.contains(location)
+        {
+            isFingerOnPaddle = true
+        }
     }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        let location = touches.first!.location(in: self)
+        if isFingerOnPaddle == true
+        {
+            paddle.position = CGPoint(x: location.x, y: paddle.position.y)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        isFingerOnPaddle = false
+    }
+    
+    
     
     func makeNewBall(loc: CGPoint)
     {
